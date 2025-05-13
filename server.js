09f853b2c7b411a920d049ca340ff354495b0be3
Ansh <anshsavadatti@gmail.com>
2025-05-13 23:45:57 +0530

    changing port to 5000

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post("/api/compare", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
